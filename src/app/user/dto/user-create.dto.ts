83239f7a465bf507137b734d2a0248aa7db2d934
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2024-05-23 19:56:32 -0300

    🐞 fix: Missing fields UserDTO

--- src/app/user/dto/user-create.dto.ts
+++ src/app/user/dto/user-create.dto.ts
@@ -2,10 +2,10 @@ import { Status } from "@prisma/client";
 import { AddressCreateDTO } from "src/app/address/dto/address-create.dto";
 
 export class UserCreateDTO{
-    id: string;
     cpf: string;
     name: string;
     birth: Date;
     address: AddressCreateDTO;
     status: Status;
+    createdBy: string;
 }
