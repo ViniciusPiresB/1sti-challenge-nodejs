000755b64e7094cad331777b74a697963d0a3e96
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2024-05-23 20:07:43 -0300

    ✨ feat: Create user

--- src/app/user/user.controller.ts
+++ src/app/user/user.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 import { UserService } from "./user.service";
+import { UserCreateDTO } from "./dto/user-create.dto";
 
 @Controller("user")
 export class UserController {
     constructor(private readonly userService: UserService) {}
+
+    @Post()
+    create(@Body() userCreateDTO: UserCreateDTO) {
+        return this.userService.create(userCreateDTO);
+    }
 }
