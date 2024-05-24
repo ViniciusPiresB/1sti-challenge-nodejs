760067acddf857fd8c1d96d34c4238184d1c0b17
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2024-05-24 15:48:46 -0300

    🐞 fix: User entity to DTO

--- src/app/user/user.service.spec.ts
+++ src/app/user/user.service.spec.ts
@@ -4,84 +4,50 @@ import { UserCreateDTO } from "./dto/user-create.dto";
 import { UserService } from "./user.service";
 import { Test, TestingModule } from "@nestjs/testing";
 import { Status, User } from "@prisma/client";
+import { UserDTO } from "./dto/user.dto";
 
 describe("UserService", () => {
   let userService: UserService;
   let prismaService: PrismaService;
 
-  const fakeUsers: User[] = [
+  const fakeUsers: UserDTO[] = [
     {
       id: "a3718843-5456-4482-9c97-a20f78cbd44e",
       cpf: "70031242546",
       name: "Test User 1",
       birth: new Date(),
-      addressId: 1,
-      status: Status.ACTIVE,
-      createdAt: new Date(),
-      createdBy: "Admin",
-      updatedAt: undefined,
-      updatedBy: undefined,
-      deletedAt: undefined,
-      deletedBy: undefined
+      status: Status.ACTIVE
     },
     {
       id: "a3718843-5456-4482-9c97-a20f78cbd45t",
       cpf: "70031242547",
       name: "Test User 2",
       birth: new Date(),
-      addressId: 2,
-      status: Status.ACTIVE,
-      createdAt: new Date(),
-      createdBy: "Admin",
-      updatedAt: undefined,
-      updatedBy: undefined,
-      deletedAt: undefined,
-      deletedBy: undefined
+      status: Status.ACTIVE
     },
     {
       id: "a3718843-5456-4482-9c97-a20f78cbd48o",
       cpf: "70031242548",
       name: "Test User 3",
       birth: new Date(),
-      addressId: 3,
-      status: Status.ACTIVE,
-      createdAt: new Date(),
-      createdBy: "Admin",
-      updatedAt: undefined,
-      updatedBy: undefined,
-      deletedAt: undefined,
-      deletedBy: undefined
+      status: Status.ACTIVE
     }
   ];
 
-  const updatedFakeUser: User = {
+  const updatedFakeUser: UserDTO = {
     id: "a3718843-5456-4482-9c97-a20f78cbd44e",
     cpf: "70031242546",
     name: "Updated Test User",
     birth: new Date(),
-    addressId: 1,
-    status: Status.ACTIVE,
-    createdAt: new Date(),
-    createdBy: "Admin",
-    updatedAt: new Date(),
-    updatedBy: "Admin",
-    deletedAt: undefined,
-    deletedBy: undefined
+    status: Status.ACTIVE
   };
 
-  const deletedFakeUser: User = {
+  const deletedFakeUser: UserDTO = {
     id: "a3718843-5456-4482-9c97-a20f78cbd44e",
     cpf: "70031242546",
     name: "Deleted Test User",
     birth: new Date(),
-    addressId: 1,
-    status: Status.DELETED,
-    createdAt: new Date(),
-    createdBy: "Admin",
-    updatedAt: new Date(),
-    updatedBy: "Admin",
-    deletedAt: new Date(),
-    deletedBy: "Admin"
+    status: Status.DELETED
   };
 
   const prismaMock = {
