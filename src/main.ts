3255d4df1f6e36e4ad374fb0c134d24955082112
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2024-05-24 13:58:16 -0300

    ✨ feat: Swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { PrismaClientExceptionFilter } from './prisma-exception.filter.ts/prisma-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +10,18 @@ async function bootstrap() {
   const { httpAdapter } = app.get(HttpAdapterHost);
 
   app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+
+  app.useGlobalPipes(new ValidationPipe({ forbidUnknownValues: false }));
+  
+  const config = new DocumentBuilder()
+    .setTitle("User CRUD")
+    .setDescription("An API for a challenge")
+    .setVersion("0.1")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup("doc", app, document);
 
   await app.listen(3000);
 }
