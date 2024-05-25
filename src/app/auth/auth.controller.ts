8f2b137d5746e8cf96aa87213103d8b25ee2e14b
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2024-05-25 19:05:18 -0300

    ✨ feat: Use guard in auth

--- src/app/auth/auth.controller.ts
+++ src/app/auth/auth.controller.ts
@@ -1,11 +1,14 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { LoginDto } from "./dto/login.dto";
+import { LocalGuard } from "./guards/local.guard";
 
 @Controller("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
   @Post("login")
+  @UseGuards(LocalGuard)
   login(@Body() loginDto: LoginDto) {
     return this.authService.validate(loginDto);
   }
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,6 +5,8 @@ import { PassportModule } from "@nestjs/passport";
 import { JwtModule } from "@nestjs/jwt";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
+import { LocalStrategy } from "./strategy/local.strategy";
+import { JwtStrategy } from "./strategy/jwt.strategy";
 
 @Module({
   imports: [
@@ -14,6 +16,6 @@ import { AuthService } from "./auth.service";
     JwtModule.register({ privateKey: process.env.JWT_SECRET })
   ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, LocalStrategy, JwtStrategy]
 })
 export class AuthModule {}
