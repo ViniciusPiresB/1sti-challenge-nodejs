8abe60061cb6c528719c12e32b96d034a0199fde
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2024-05-24 23:06:11 -0300

    🦄 refactor: Import module Address

--- src/app/address/address.module.ts
+++ src/app/address/address.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { PrismaService } from "src/database/prisma.service";
 import { AddressService } from "./address.service";
 import { UserService } from "../user/user.service";
+import { AddressController } from "./address.controller";
 
 @Module({
-  controllers: [],
-  providers: [PrismaService, UserService],
+  controllers: [AddressController],
+  providers: [PrismaService, UserService, AddressService],
   exports: [AddressService]
 })
 export class AddressModule {}
