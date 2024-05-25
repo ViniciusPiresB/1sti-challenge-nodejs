30ba240b45309e07f2a6ce862329e2382e059d6e
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2024-05-24 22:53:48 -0300

    ✨ feat: Find address by user

--- src/app/address/address.module.ts
+++ src/app/address/address.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 import { PrismaService } from "src/database/prisma.service";
 import { AddressService } from "./address.service";
+import { UserService } from "../user/user.service";
 
 @Module({
   controllers: [],
-  providers: [PrismaService],
+  providers: [PrismaService, UserService],
   exports: [AddressService]
 })
 export class AddressModule {}
--- src/app/address/address.service.ts
+++ src/app/address/address.service.ts
@@ -1,7 +1,33 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { PrismaService } from "src/database/prisma.service";
+import { UserService } from "../user/user.service";
+import { Address } from "@prisma/client";
+import { AddressDTO } from "./dto/address.dto";
 
 @Injectable()
 export class AddressService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly userService: UserService
+  ) {}
+
+  private async findAddressByUser(cpf: string) {
+    const user = await this.userService.findOne(cpf);
+
+    const address = await this.prismaService.address.findUnique({
+      where: { userId: user.id }
+    });
+
+    if (!address) throw new NotFoundException("User not found.");
+
+    return this.convertToAddressDTO(address);
+  }
+
+  private convertToAddressDTO(address: Address) {
+    const { id, userId, ...rest } = address;
+
+    const addressDTO: AddressDTO = rest;
+
+    return addressDTO;
+  }
 }
