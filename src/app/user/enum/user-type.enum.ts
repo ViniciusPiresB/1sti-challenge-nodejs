ee8018a6e6cfaa46ef2ba11c7cd5281430d9c0b2
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2024-05-25 19:00:42 -0300

    ✨ feat: UserTypes

--- src/app/user/enum/user-type.enum.ts
+++ src/app/user/enum/user-type.enum.ts
@@ -3,7 +3,3 @@ export enum UserType {
   Admin = 1,
   Root = 2
 }
-
-export const UserRole = [UserType.User, UserType.Admin, UserType.Root];
-export const AdminRole = [UserType.Admin, UserType.Root];
-export const RootRole = [UserType.Root];
