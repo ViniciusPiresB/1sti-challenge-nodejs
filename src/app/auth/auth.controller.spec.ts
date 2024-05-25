f3bc633de93fab0c3183d272e1a9d88ce7b0db5a
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2024-05-25 20:06:43 -0300

    🧪 test: login controller test

--- src/app/auth/auth.controller.spec.ts
+++ src/app/auth/auth.controller.spec.ts
@@ -2,6 +2,7 @@ import { Address, Status } from "@prisma/client";
 import { Test, TestingModule } from "@nestjs/testing";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
+import { LoginDto } from "./dto/login.dto";
 
 describe("AuthController", () => {
   let authController: AuthController;
@@ -27,5 +28,20 @@ describe("AuthController", () => {
 
   it("Should be defined", () => {
     expect(authController).toBeDefined();
+  });
+
+  describe("login", () => {
+    it("Should return token", async () => {
+      const loginDto: LoginDto = {
+        cpf: "12345678910",
+        password: "password"
+      };
+
+      const result = await authController.login(loginDto);
+
+      expect(result).toEqual({ accessToken: "fake token" });
+      expect(authServiceMock.validate).toHaveBeenCalledWith(loginDto);
+      expect(authServiceMock.validate).toHaveBeenCalledTimes(1);
+    });
   });
 });
