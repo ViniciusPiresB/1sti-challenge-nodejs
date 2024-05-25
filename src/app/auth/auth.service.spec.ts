3774ecc3db88bd1c58b685c14514425a4c9186d5
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2024-05-25 19:58:04 -0300

    🧪 test: Validate Auth test

--- src/app/auth/auth.service.spec.ts
+++ src/app/auth/auth.service.spec.ts
@@ -3,6 +3,9 @@ import { AuthService } from "./auth.service";
 import { Test, TestingModule } from "@nestjs/testing";
 import { UserService } from "../user/user.service";
 import { Status } from "@prisma/client";
+import { validatePassword } from "../utils/validate-password";
+import { LoginDto } from "./dto/login.dto";
+import { BadRequestException, UnauthorizedException } from "@nestjs/common";
 
 jest.mock("../utils/validate-password");
 
@@ -47,5 +50,61 @@ describe("AuthService", () => {
     expect(authService).toBeDefined();
     expect(userService).toBeDefined();
     expect(jwtService).toBeDefined();
+  });
+
+  describe("validate", () => {
+    const loginDto: LoginDto = {
+      cpf: "12345678910",
+      password: "password"
+    };
+
+    const user = {
+      password: "1234",
+      id: "asdsa-ewrwer",
+      cpf: "12345678910",
+      name: "Fake User",
+      birth: new Date(),
+      status: Status.ACTIVE,
+      typeUser: 0
+    };
+
+    it("Should return a accessToken if user is valid", async () => {
+      (validatePassword as jest.Mock).mockResolvedValueOnce(true);
+      mockJwtService.sign.mockReturnValueOnce("fake token");
+
+      const result = await authService.validate(loginDto);
+
+      expect(result).toEqual({ accessToken: "fake token" });
+    });
+
+    it("Should throw Error if password or cpf is missing", async () => {
+      await expect(
+        authService.validate({ ...loginDto, password: "" })
+      ).rejects.toThrow(
+        new BadRequestException("Missing password or cpf in request.")
+      );
+
+      await expect(
+        authService.validate({ ...loginDto, cpf: "" })
+      ).rejects.toThrow(
+        new BadRequestException("Missing password or cpf in request.")
+      );
+    });
+
+    it("Should throw Error if user is not found or password invalid", async () => {
+      mockUserService.getUserWithPassword.mockResolvedValue(null);
+      (validatePassword as jest.Mock).mockResolvedValue(false);
+
+      await expect(authService.validate(loginDto)).rejects.toThrow(
+        new UnauthorizedException("Email or password invalid.")
+      );
+
+      mockUserService.getUserWithPassword.mockResolvedValue(user);
+      (validatePassword as jest.Mock).mockResolvedValue(false);
+
+      await expect(authService.validate(loginDto)).rejects.toThrow(
+        new UnauthorizedException("Email or password invalid.")
+      );
+    });
   });
 });
