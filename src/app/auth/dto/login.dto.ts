3ffc24eaef2f5ccc60f219426ad4e8d10271b1da
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2024-05-26 09:53:46 -0300

    📃 docs: Add swagger on login

--- src/app/auth/dto/login.dto.ts
+++ src/app/auth/dto/login.dto.ts
@@ -1,4 +1,26 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString } from "class-validator";
+
 export class LoginDto {
+  @ApiProperty({
+    title: "CPF of user to login",
+    type: String,
+    description: "This is a required property",
+    example: "12345678910",
+    required: true
+  })
+  @IsString()
+  @IsNotEmpty()
   cpf: string;
+
+  @ApiProperty({
+    title: "Password of user to login",
+    type: String,
+    description: "This is a required property",
+    example: "pass123",
+    required: true
+  })
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
